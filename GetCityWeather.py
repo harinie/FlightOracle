ceaee0a88564a6e3cb286589276d7e3a8eb64de9
Harini Eavani <harinie@gmail.com>
2015-07-07 13:16:00 -0400

    updated files

--- GetCityWeather.py
+++ GetCityWeather.py
@@ -3,11 +3,16 @@ import json
 
 def GetCityWeather(city,state,date):
 
+    city = city.replace(' ','_')
+    if date>3:
+        date=0
+        
     f = urllib2.urlopen('http://api.wunderground.com/api/acab7566950fd543/forecast/q/'+state+'/'+city+'.json')
     json_string = f.read()
     parsed_json = json.loads(json_string)
     qpf_allday = parsed_json['forecast']['simpleforecast']['forecastday'][date]['qpf_allday']['mm']
-    print "Current temperature is: %s" % (qpf_allday)
+    forecasttext = parsed_json['forecast']['txt_forecast']['forecastday'][date]['fcttext_metric']
+    #print "Current temperature is: %s" % (qpf_allday)
     f.close()
     
-    return temp_f
+    return forecasttext
--- routes.py
+++ routes.py
@@ -1,16 +1,34 @@
 from flask import Flask, render_template, json, request, jsonify
 import numpy as np
 import pandas as pd
+import time
 
-#from GetCityWeather import GetCityWeather
+from GetCityWeather import GetCityWeather
 
-# Setting up matplotlib sytles using BMH
-s = json.load(open("./static/bmh_matplotlibrc.json"))
 
 airport_data = pd.read_csv('./airports.csv',header=0,quotechar='"',sep=',',na_values = ['NA', '-', '.', ''])
 
 app = Flask(__name__)
 app.Debug = True
+
+########### calculations
+def makeFeatureVector(data):
+    testVector = np.zeros(2)
+    return testVector
+    
+def predictFlightStatus(depid,arrid,depdate,deptime):
+    daysfromNow = int(depdate.split('-')[1]) - int(time.strftime("%d"))
+    depforetext=GetCityWeather(airport_data.ix[depid]["city"],airport_data.ix[depid]["state"],daysfromNow)
+    arrforetext=GetCityWeather(airport_data.ix[arrid]["city"],airport_data.ix[arrid]["state"],daysfromNow)
+   # testVector = makeFeatureVector(data)
+    #depwe="Sunny"
+    #arrwe="Rain"
+    cancellation=50
+    delay=60
+    return depforetext,arrforetext,cancellation,delay
+
+
+########## App related
 
 # log to stderr
 import logging
@@ -28,9 +46,15 @@ def home():
 def query():
     data = json.loads(request.data)
     app.logger.debug(data['depdate'])
+    data['departure'] = data['departure'].upper()
+    data['arrival'] = data['arrival'].upper()    
     departure_id = np.where(airport_data["iata"]==data['departure'])[0][0]
     arrival_id = np.where(airport_data["iata"]==data['arrival'])[0][0]    
-    return jsonify(deplat=airport_data.ix[departure_id]["lat"],deplong=airport_data.ix[departure_id]["long"],arrlat=airport_data.ix[arrival_id]["lat"],arrlong=airport_data.ix[arrival_id]["long"])
+    depwe,arrwe,cancellation,delay = predictFlightStatus(departure_id,arrival_id,data['depdate'],data['deptime'])
+    return jsonify(deplat=airport_data.ix[departure_id]["lat"],deplong=airport_data.ix[departure_id]["long"],
+                   arrlat=airport_data.ix[arrival_id]["lat"],arrlong=airport_data.ix[arrival_id]["long"],
+                    depwe="Weather at departure: "+depwe,arrwe="Weather at arrival: "+arrwe,
+                    cancellation="Chance of cancellation: "+str(cancellation)+'%',delay="Expected delay: "+str(delay)+'min')
     
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
