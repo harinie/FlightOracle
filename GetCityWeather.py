f14391de42c359575c1139b77724766064470285
Harini Eavani <harinie@gmail.com>
2015-07-13 13:56:40 -0400

    added model files

--- GetCityWeather.py
+++ GetCityWeather.py
@@ -1,6 +1,8 @@
 import urllib2
 import json
+import numpy as np
 
+   
 def GetCityWeather(city,state,date):
 
     city = city.replace(' ','_')
@@ -10,9 +12,29 @@ def GetCityWeather(city,state,date):
     f = urllib2.urlopen('http://api.wunderground.com/api/acab7566950fd543/forecast/q/'+state+'/'+city+'.json')
     json_string = f.read()
     parsed_json = json.loads(json_string)
-    qpf_allday = parsed_json['forecast']['simpleforecast']['forecastday'][date]['qpf_allday']['mm']
-    forecasttext = parsed_json['forecast']['txt_forecast']['forecastday'][date]['fcttext_metric']
-    #print "Current temperature is: %s" % (qpf_allday)
-    f.close()
     
-    return forecasttext
+    forecasttext = parsed_json['forecast']['txt_forecast']['forecastday'][date]['fcttext_metric']
+    
+    # make the vector
+    discrete_vals = []
+    cont_vals = []
+
+    #"Mean TemperatureF"
+    high = float(parsed_json['forecast']['simpleforecast']['forecastday'][date]['high']['fahrenheit'])
+    low = float(parsed_json['forecast']['simpleforecast']['forecastday'][date]['low']['fahrenheit'])
+    cont_vals.append(0.5*(high + low))
+    
+    # mean humidity
+    cont_vals.append(float(parsed_json['forecast']['simpleforecast']['forecastday'][date]['avehumidity']))
+    cont_vals.append(float(parsed_json['forecast']['simpleforecast']['forecastday'][date]['maxwind']['mph']))
+    cont_vals.append(float(parsed_json['forecast']['simpleforecast']['forecastday'][date]['avewind']['mph']))
+    # precipitationIn
+    cont_vals.append(float(parsed_json['forecast']['simpleforecast']['forecastday'][date]['qpf_allday']['in']))
+    
+        # wind dir degrees
+    windDirDegrees = float(parsed_json['forecast']['simpleforecast']['forecastday'][date]['avewind']['degrees'])
+    discrete_vals = np.floor(windDirDegrees/30 )
+    
+    f.close()
+
+    return forecasttext,discrete_vals,cont_vals
--- routes.py
+++ routes.py
@@ -1,41 +1,90 @@
 from flask import Flask, render_template, json, request, jsonify
 import numpy as np
 import pandas as pd
-import time
-
+import time,datetime
 from GetCityWeather import GetCityWeather
+# log to stderr
+import logging
+from logging import StreamHandler
+file_handler = StreamHandler()
+
 
 
 airport_data = pd.read_csv('./airports.csv',header=0,quotechar='"',sep=',',na_values = ['NA', '-', '.', ''])
+discrete_encode_file='model/discrete_encode.txt'
+imputer_file='model/imputer.npy'
+scale_file='model/scaler.npy'
+clf_file='model/clf.npy'
+reg_file='model/reg.npy'
 
+imputer = np.load(imputer_file)
+scaler = np.load(scale_file)
+clf = np.load(clf_file)
+reg = np.load(reg_file)
+cat_keys=[]
+with open(discrete_encode_file) as f:
+    for line in f:
+        cat_keys.append(line.strip())
+f.close()
+    
 app = Flask(__name__)
 app.Debug = True
-
+app.logger.setLevel(logging.DEBUG)  # set the desired logging level here
+app.logger.addHandler(file_handler)
 ########### calculations
 def makeFeatureVector(data):
     testVector = np.zeros(2)
     return testVector
     
-def predictFlightStatus(depid,arrid,depdate,deptime):
-    daysfromNow = int(depdate.split('-')[1]) - int(time.strftime("%d"))
-    depforetext=GetCityWeather(airport_data.ix[depid]["city"],airport_data.ix[depid]["state"],daysfromNow)
-    arrforetext=GetCityWeather(airport_data.ix[arrid]["city"],airport_data.ix[arrid]["state"],daysfromNow)
-   # testVector = makeFeatureVector(data)
-    #depwe="Sunny"
-    #arrwe="Rain"
-    cancellation=50
-    delay=60
+def predictFlightStatus(depid,arrid,depdate,deptime,carrier):
+
+    dateval = datetime.date(int(depdate.split('-')[0]),int(depdate.split('-')[1]),int(depdate.split('-')[2]))
+    dep_disc_vector = []
+
+    dep_disc_vector.append('MONTH='+str(int(depdate.split('-')[1])))
+    dep_disc_vector.append('DAY_OF_WEEK='+str(dateval.weekday()+1))
+    hour = float(deptime.split(':')[0])
+    #if deptime.split(' ')[1]:
+    #    hour = hour + 12
+    dep_disc_vector.append('CRS_DEP_TIME='+str(hour)) 
+    dep_disc_vector.append('CARRIER='+carrier) 
+    
+    daysfromNow = int(depdate.split('-')[2]) - int(time.strftime("%d"))
+    depforetext,dep_disc_we,dep_cont_we=GetCityWeather(airport_data.ix[depid]["city"],airport_data.ix[depid]["state"],daysfromNow)
+    arrforetext,arr_disc_we,arr_cont_we=GetCityWeather(airport_data.ix[arrid]["city"],airport_data.ix[arrid]["state"],daysfromNow)
+    
+    dep_disc_vector.append('WindDirDegrees='+str(dep_disc_we))
+    dep_disc_vector.append('WindDirDegrees_DEST='+str(arr_disc_we))
+    
+    # first encode categorical variables
+    disc_features = np.zeros(len(cat_keys))
+    for entry in dep_disc_vector:
+        found_ind = [e for e,x in enumerate(cat_keys) if entry==x]
+        disc_features[found_ind[0]]=1
+    
+    all_features=np.concatenate((dep_cont_we, arr_cont_we, disc_features), axis=1)
+    # then inpute
+    for e,entry in enumerate(all_features):
+        if np.isnan(entry):
+            all_features[e] = imputer[e]
+            
+    # now scale
+    for e,entry in enumerate(all_features):
+        all_features[e] = all_features[e] - scaler[e] # demean
+        all_features[e] = 1.0*all_features[e]/scaler[e+all_features.shape[0]] # scale
+
+    #now predict
+    prediction = np.dot(all_features,clf[0:all_features.shape[0]]) + clf[-1]
+    prediction = np.exp(-1.0*prediction)
+    cancellation=int(100/(1+prediction))
+    
+    #
+    prediction = np.dot(all_features,reg[0:all_features.shape[0]]) + reg[-1]
+    delay=int(prediction)
     return depforetext,arrforetext,cancellation,delay
 
 
 ########## App related
-
-# log to stderr
-import logging
-from logging import StreamHandler
-file_handler = StreamHandler()
-app.logger.setLevel(logging.DEBUG)  # set the desired logging level here
-app.logger.addHandler(file_handler)
 
 @app.route('/')
 def home():
@@ -45,12 +94,11 @@ def home():
 @app.route('/query', methods=['POST'])
 def query():
     data = json.loads(request.data)
-    app.logger.debug(data['depdate'])
     data['departure'] = data['departure'].upper()
     data['arrival'] = data['arrival'].upper()    
     departure_id = np.where(airport_data["iata"]==data['departure'])[0][0]
     arrival_id = np.where(airport_data["iata"]==data['arrival'])[0][0]    
-    depwe,arrwe,cancellation,delay = predictFlightStatus(departure_id,arrival_id,data['depdate'],data['deptime'])
+    depwe,arrwe,cancellation,delay = predictFlightStatus(departure_id,arrival_id,data['depdate'],data['deptime'],data['carrier'])
     return jsonify(deplat=airport_data.ix[departure_id]["lat"],deplong=airport_data.ix[departure_id]["long"],
                    arrlat=airport_data.ix[arrival_id]["lat"],arrlong=airport_data.ix[arrival_id]["long"],
                     depwe="Weather at departure: "+depwe,arrwe="Weather at arrival: "+arrwe,
